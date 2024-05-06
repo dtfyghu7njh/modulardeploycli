33900487dad79e8723e2435ece597a963e0ee12f
dtfyghu7njh <js1tamarafw@hotmail.com>
2024-05-06 10:23:18 +0700

    aptos task

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-function searchInsert(nums, target) {
-  let left = 0;
-  let right = nums.length - 1;
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-    if (nums[mid] === target) return mid;
-    if (nums[mid] < target) left = mid + 1;
-    else right = mid - 1;
+const fibonacci = (n) => {
+  const sequence = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    sequence.push(sequence[i - 1] + sequence[i - 2]);
   }
-  return left;
-}
+  return sequence;
+};
