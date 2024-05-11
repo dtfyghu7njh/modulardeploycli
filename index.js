ab8f3cc758d1de612f8ca8d403a425c2cec2dc08
dtfyghu7njh <js1tamarafw@hotmail.com>
2024-05-12 05:46:16 +0700

    update sdk commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const shuffle = (arr) => arr.sort(() => Math.random() - 0.5);
+const pullAtIndex = (arr, pullArr) => {
+  let removed = [];
+  let pulled = arr
+    .map((v, i) => (pullArr.includes(i) ? removed.push(v) : v))
+    .filter((v, i) => !pullArr.includes(i));
+  arr.length = 0;
+  pulled.forEach((v) => arr.push(v));
+  return removed;
+};
